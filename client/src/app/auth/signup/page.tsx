261caf3c4f55c45804e989ebab8b60e48ac3083d
Abdeltif <abdeltif.boujamza@gmail.com>
2023-12-05 04:17:35 +0100

    add sign up static page

--- client/src/app/auth/signup/page.tsx
+++ client/src/app/auth/signup/page.tsx
@@ -1,3 +1,50 @@
-export default async function Page() {
-  return <div className="flex flex-col items-center p-4 gap-4">signup</div>;
+import { Metadata } from "next";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Sign up",
+};
+
+export default function SignupPage() {
+  return (
+    <div className="relative flex flex-col justify-center items-center min-h-screen overflow-hidden">
+      <div className="w-full m-auto bg-white lg:max-w-lg">
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl text-center">Sign in</CardTitle>
+            <CardDescription className="text-center">Enter your email and password to login</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="example@email.com" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" placeholder="********" />
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col">
+            <Button className="w-full">Login</Button>
+            <p className="mt-2 text-xs text-center text-gray-700">
+              <Link href="/reset" className="underline text-primary">
+                Forgot password?
+              </Link>
+            </p>
+
+            <p className="mt-2 text-xs text-center text-gray-700">
+              Don't have an account?{" "}
+              <Link href="/reset" className="underline text-primary">
+                Sign up
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
 }
